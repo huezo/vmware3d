4e59523acbbbd37ae9806bb8d6489d284fcc2328
HUEZO <huezo@users.noreply.github.com>
2017-04-15 11:53:36 -0600

    v1.0
    v1.0

---
+++ vmware3d.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo "Habilita la aceleración  3D en  VMWare"
+
+echo ' mks.gl.allowBlacklistedDrivers = "TRUE" ' >> ~/.vmware/preferences 
+
+echo " terminado con exito"
+
+
+echo "https://huezohuezo1990.wordpress.com"
+
+
+
